6d0b1d34f2b84bcc7569e82cf25103e89422cbfa
IgnacioBerrocalA <gncberrocal@gmail.com>
2026-02-19 10:07:51 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,64 +5,14 @@
 
 using namespace std;
 
-int menu() {
-    int opcion;
-    cout<<endl;
-    cout<<"===== JARDIN ====="<<endl;
-    cout<<"1. Agregar arbol"<<endl;
-    cout<<"2. Agregar flor"<<endl;
-    cout<<"3. Mostrar jardin"<<endl;
-    cout<<"0. Salir"<<endl;
-    cout<<"Opcion: ";
-    cin>>opcion;
-    return opcion;
-}
-
 int main() {
+
     Jardin jardin(800,600);
 
-    int opcion;
-    int x,y,t;
+    LectorArchivo lector("datos.txt");
+    lector.cargar(jardin);
 
-    do {
-
-        opcion = menu();
-
-        switch(opcion) {
-
-        case 1:
-            cout<<"Posicion x: ";
-            cin>>x;
-            cout<<"Posicion y: ";
-            cin>>y;
-            cout<<"Altura: ";
-            cin>>t;
-            jardin.agregar(new Arbol(x,y,t));
-            break;
-
-        case 2:
-            cout<<"Posicion x: ";
-            cin>>x;
-            cout<<"Posicion y: ";
-            cin>>y;
-            cout<<"Tamano: ";
-            cin>>t;
-            jardin.agregar(new Flor(x,y,t));
-            break;
-
-        case 3:
-            jardin.mostrar();
-            break;
-
-        case 0:
-            cout<<"Saliendo..."<<endl;
-            break;
-
-        default:
-            cout<<"Opcion invalida"<<endl;
-        }
-
-    } while(opcion!=0);
+    jardin.mostrar();
 
     return 0;
 }
